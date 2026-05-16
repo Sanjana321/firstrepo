0576ce48960032390a23a33d88a1a94e4833a1a4
Sanjana Dubey <121307170+Sanjana321@users.noreply.github.com>
2026-05-16 16:39:31 +0530

    Update print message in firstcode class

--- print.java
+++ print.java
@@ -1,6 +1,6 @@
 // Print some thing
 public class firstcode {
     public static void main(String[] args) {
-        System.out.println("This is my first code");
+        System.out.println("This is merge code");
     }
 }
