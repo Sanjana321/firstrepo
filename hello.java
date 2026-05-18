8648fb0c77fe8d177b3ed566a5591685bce7fce2
Sanjana Dubey <121307170+Sanjana321@users.noreply.github.com>
2026-05-18 12:28:45 +0530

    Update output message in hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 // Print some thing
 public class hello {
     public static void main(String[] args) {
-        System.out.println("This is my first code");
+        System.out.println("This is my hello code");
     }
 }
